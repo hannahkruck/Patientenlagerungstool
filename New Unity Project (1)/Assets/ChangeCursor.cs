dc72c35e632eac37cec78e3988a8be0afde7879c
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2021-05-09 21:13:40 +0200

    Stand 07.05. - Menüleiste

---
+++ New Unity Project (1)/Assets/ChangeCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    //public Texture2D editcursor;
+   
+    void start()
+    {
+        Cursor.visible = false;
+        //Cursor.SetCursor(editcursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    
+}
---
+++ New Unity Project (1)/Assets/Success.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Success : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    bool active;
+
+    public void Successmessage()
+    {
+        if (active == false)
+        {
+            gameObject.transform.gameObject.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            gameObject.transform.gameObject.SetActive(false);
+            active = false;
+        }
+
+    }
+
+}
