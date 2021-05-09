14bbf4b388a4374a9d1092599814d371ef940a0f
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2021-05-09 21:34:12 +0200

    Neues Input System
    Menü nicht mehr ein- und ausklappbar

---
+++ New Unity Project (1)/Assets/ClosePanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ClosePanel : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    bool active;
+
+    public void OpenAndClose()
+    {
+        if (active == false)
+        {
+            gameObject.transform.gameObject.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            gameObject.transform.gameObject.SetActive(false);
+            active = false;
+        }
+
+    }
+
+}
