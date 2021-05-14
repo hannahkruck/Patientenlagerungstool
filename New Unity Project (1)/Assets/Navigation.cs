1037c3925c8a0a61d9af47571398d5d7cab352ef
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2021-05-14 15:42:12 +0200

    UI Changes

---
+++ New Unity Project (1)/Assets/Navigation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class Navigation : MonoBehaviour
+
+{
+
+    [SerializeField] GameObject[] panels;
+
+
+
+    public void NavigationBarClick(GameObject activePanel)
+
+    {
+
+        for (int i = 0; i < panels.Length; i++)
+
+        {
+
+            panels[i].SetActive(false);
+
+        }
+
+        activePanel.SetActive(true);
+
+    }
+
+}
